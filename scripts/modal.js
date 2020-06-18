4558c2bf46d393d2cfbc29043313fca38c5a601c
jadiscke <vjtasso@gmail.coom>
2020-06-18 15:54:59 -0300

    Add Dynamic Image to Modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -3,10 +3,12 @@ const cards = document.querySelectorAll('.cards');
 
 const modalTitle = modalOverlay.querySelector('h3');
 const modalAuthor = modalOverlay.querySelector('span');
+const modalImg = modalOverlay.querySelector('img');
 for (const card of cards) {
 
   card.addEventListener('click', (event) => {
     const targetCard = event.target;
+    modalImg.src = targetCard.querySelector('img').src;
     modalTitle.innerHTML = targetCard.querySelector('.card-name').innerHTML;
     modalAuthor.innerHTML =targetCard.querySelector('.card-author').innerHTML;
     modalOverlay.classList.add('active');
