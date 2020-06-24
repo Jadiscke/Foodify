e39e286ec2522721347c94a207e6c642b47e6ecc
jadiscke <vjtasso@gmail.coom>
2020-06-24 10:29:23 -0300

    Add Dependencies and Configure basic server

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const nunjucks= require('nunjucks');
+
+const server = express();
+
+const port = process.env.PORT || 3000;
+const nunjucksConfigurations = {
+  express: server,
+  autoescape: true,
+  noChace: true
+}
+
+server.use(express.static('public'));
+server.set('view engine', 'njk');
+nunjucks.configure(nunjucksConfigurations);
+// routes
+
+server.get('/', (req,res) => {
+  return res.render('index')
+});
+
+server.get('/about', (req,res) => {
+  return res.render('about')
+});
+
+server.get('/recipes', (req, res) => {
+  return res.render('/recipes');
+});
+
+server.listen(port, () => {
+  console.log(`Listening on PORT: ${3000}`);
+});
+
+
