68d3f91dc15a1aa65a906e73ae8fc40faacd193b
jadiscke <vjtasso@gmail.coom>
2020-07-20 11:41:21 -0300

    Create Persintence and Modify Form

--- src/admin.js
+++ src/admin.js
@@ -1,14 +1,29 @@
-const data = require('./data');
+const data = require('./data.json');
+const fs = require('fs');
 
 
 exports.index = (req, res) => {
-  const dataIndex = data;
+  const dataIndex = data.recipes;
   res.render('admin/index', {dataIndex});
 }
 
 exports.show =  (req,res) => {
-  const food = data[parseInt(req.params.id)];
-  return res.render('admin/recipe', {food});
+  const { id } = req.params;
+  const foundRecipe = data.recipes.find(function(recipe){
+    return recipe.id == id
+  });
+
+  if(!foundRecipe){
+    return res.send("RECIPE NOT FOUND");
+  }
+  
+  
+
+  const recipe = {
+    ...foundRecipe,
+
+  }
+  return res.render('recipes/show', {food: recipe});
 }
 
 exports.create =  (req, res) => {
@@ -16,5 +31,30 @@ exports.create =  (req, res) => {
 }
 
 exports.post =  (req, res) => {
-  res.send(req.body);
+
+  const {image_url, title, ingredients,author, preparations, information} = req.body;
+
+  const lastRecipe = data.recipes[data.recipes.length - 1];
+  const id = lastRecipe ?  lastRecipe.id + 1 : 1;
+
+  const data_send = {
+    id,
+    title,
+    author,
+    image_url,
+    ingredients,
+    preparations,
+    information
+  }
+  
+
+  data.recipes.push(data_send);
+  
+
+  fs.writeFile("data.json",JSON.stringify(data, null, 2), function(err){
+    if(err) return res.send('Write File Error');
+    console.log(data);    
+    res.redirect('/admin');
+  });
+  
 }
