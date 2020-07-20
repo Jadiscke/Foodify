5045c35c748324d6a32c5fda2611bb69a8f398be
jadiscke <vjtasso@gmail.coom>
2020-07-20 10:56:00 -0300

    Add Redirection for Admin page

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,9 @@ routes.get('/about',client.about);
 
 routes.get('/recipes',client.recipes);
 routes.get('/recipes/:index', client.recipe);
-
+routes.get('/admin', (req,res) => {
+  return res.redirect('/admin/recipres')
+});
 routes.get("/admin/recipes", admin.index);
 routes.get("/admin/recipes/create", admin.create); // Mostrar formulário de nova receita
 routes.get("/admin/recipes/:id", admin.show); // Exibir detalhes de uma receita
