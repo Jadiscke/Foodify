61b5a72161aab06a34085bf7daea619a07d51311
Jadiscke <vjtasso@gmail.com>
2020-08-17 16:18:07 -0300

    Fix Chosen Script for all cases

--- public/scripts/chosen.js
+++ public/scripts/chosen.js
@@ -4,8 +4,8 @@ function main(){
     const links = document.querySelectorAll("header div a");
 
     for (const link of links) {
-        console.log(link.href)
-        if (link.href == window.location.href){
+
+        if (window.location.href.includes(link.href)){
             link.classList.add('chosen');
         }
     }
